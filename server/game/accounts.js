05df368e88d3aa467bd300fe57f2231645802c38
meh-a <meh-a@users.noreply.github.com>
2026-04-24 15:51:53 -0700

    Create accounts.js

---
+++ server/game/accounts.js
@@ -0,0 +1,5 @@
+  export function register() { return {}; }
+  export function login()    { return {}; }                                                                                        
+  export function purchaseSlot() { return {}; }             
+  export function addGold()  { return 0; }                                                                                         
+  export function getLeaderboard() { return []; }  
